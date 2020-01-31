6d0f88afcf0f7593380628855bebf3a0c5338b54
chamatt <matheus@nextly.team>
2020-01-31 18:15:03 -0300

    Updates

---
+++ precache-manifest.22cc39209c0aeaaf7c4a6e4daa7469cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660971e0a0f91ec4db8b5a6bbafc8e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43e0b3147f5b1f892eba",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "8ddb7f6538b30adb38cd",
+    "url": "/static/css/main.8c3776c1.chunk.css"
+  },
+  {
+    "revision": "43e0b3147f5b1f892eba",
+    "url": "/static/js/2.7c07df91.chunk.js"
+  },
+  {
+    "revision": "0897a2af4d47251d787cefe2723205c2",
+    "url": "/static/js/2.7c07df91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddb7f6538b30adb38cd",
+    "url": "/static/js/main.6c8684ad.chunk.js"
+  },
+  {
+    "revision": "d634deed0ed72391907d",
+    "url": "/static/js/runtime-main.d2106eb4.js"
+  },
+  {
+    "revision": "90821be93849253197c9a5b7577013de",
+    "url": "/static/media/flame-sign-up.90821be9.png"
+  }
+]);
