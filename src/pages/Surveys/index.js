b2422aa8ba1050163b373177a0b09f99f343f3fd
Gabriel Rezende <gabriel.rezende@leanonsystems.com>
2020-02-03 18:41:43 -0300

    separate surveys

--- src/pages/Surveys/index.js
+++ src/pages/Surveys/index.js
@@ -10,6 +10,9 @@ import AuthContext from "../../contexts/auth";
 export default function Surveys() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState();
+  const [closedSurveys, setClosedSurveys] = useState();
+  const [openSurveys, setOpenSurveys] = useState();
+  const [idleSurveys, setIdleSurveys] = useState();
   const { user } = useContext(AuthContext);
 
   const fetchData = () => {
@@ -17,7 +20,9 @@ export default function Surveys() {
       .get("/surveys/")
       .then(response => {
         setLoading(false);
-        setData(response.data);
+        setClosedSurveys(response?.data?.filter(s => s.status === "CLOSED"));
+        setOpenSurveys(response?.data?.filter(s => s.status === "ACTIVE"));
+        setIdleSurveys(response?.data?.filter(s => s.status === "IDLE"));
 
         console.log(response.data);
       })
@@ -31,23 +36,65 @@ export default function Surveys() {
   return (
     <Container>
       <Header />
-      <SizedBox height="50px"></SizedBox>
+      {!loading && openSurveys?.length && (
+        <>
+          <SizedBox height="50px"></SizedBox>
+          <Title>Active Surveys</Title>
+          <SizedBox height="20px"></SizedBox>
+          <SurveyGrid>
+            {openSurveys?.map(survey => (
+              <SurveyCard
+                key={survey.id}
+                title={survey.title}
+                surveyId={survey.id}
+                numQuestions={survey.questions.length}
+                status={survey.status}
+                refetchData={fetchData}
+              />
+            ))}
+          </SurveyGrid>
+        </>
+      )}
 
-      <Title>Surveys</Title>
-      <SizedBox height="20px"></SizedBox>
-      <SurveyGrid>
-        {!loading &&
-          data?.map(survey => (
-            <SurveyCard
-              key={survey.id}
-              title={survey.title}
-              surveyId={survey.id}
-              numQuestions={survey.questions.length}
-              status={survey.status}
-              refetchData={fetchData}
-            />
-          ))}
-      </SurveyGrid>
+      {!loading && idleSurveys?.length && (
+        <>
+          <SizedBox height="50px"></SizedBox>
+          <Title>Idle Surveys</Title>
+          <SizedBox height="20px"></SizedBox>
+          <SurveyGrid>
+            {idleSurveys?.map(survey => (
+              <SurveyCard
+                key={survey.id}
+                title={survey.title}
+                surveyId={survey.id}
+                numQuestions={survey.questions.length}
+                status={survey.status}
+                refetchData={fetchData}
+              />
+            ))}
+          </SurveyGrid>
+        </>
+      )}
+
+      {!loading && closedSurveys?.length && (
+        <>
+          <SizedBox height="50px"></SizedBox>
+          <Title>Closed Surveys</Title>
+          <SizedBox height="20px"></SizedBox>
+          <SurveyGrid>
+            {closedSurveys?.map(survey => (
+              <SurveyCard
+                key={survey.id}
+                title={survey.title}
+                surveyId={survey.id}
+                numQuestions={survey.questions.length}
+                status={survey.status}
+                refetchData={fetchData}
+              />
+            ))}
+          </SurveyGrid>
+        </>
+      )}
     </Container>
   );
 }
