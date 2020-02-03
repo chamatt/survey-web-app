80ce1d8a837b7bcdcb72ec791135aa78dcff1068
chamatt <matheus@nextly.team>
2020-02-03 15:46:06 -0300

    starting create page

--- src/pages/CreateSurvey/index.js
+++ src/pages/CreateSurvey/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Container,
@@ -8,25 +8,63 @@ import {
   QuestionCard,
   QuestionItemContainer,
   QuestionTitle,
-  QuestionAction
+  QuestionAction,
+  Card,
+  Button,
+  QuestionTitleInput,
+  OptionInput,
+  OptionInputContainer,
+  I,
+  QuestionTitleInputContainer
 } from "./styles";
-import Card from "../../components/Card";
+import SizedBox from "../../components/SizedBox";
+import Input from "../../components/Input";
+
+const mock = [
+  {
+    title: "Question 1",
+    options: ["option 1", "option 2", "option 3"]
+  }
+];
 
 export default function CreateSurvey() {
+  const [questions, setQuestions] = useState([]);
+  const [selectedQuestion, setSelectedQuestion] = useState();
   return (
     <Container>
       <SideBar>
-        <SideBarItem>
-          <QuestionItemContainer>
-            <QuestionNumber>1 - </QuestionNumber>
-            <QuestionTitle>Section 1</QuestionTitle>
-          </QuestionItemContainer>
-          <QuestionAction />
-        </SideBarItem>
+        {[0, 1, 2, 3].map(() => (
+          <SideBarItem>
+            <QuestionItemContainer>
+              {/* <QuestionNumber>1 - </QuestionNumber> */}
+              <QuestionTitle>Question 1</QuestionTitle>
+            </QuestionItemContainer>
+          </SideBarItem>
+        ))}
+        <SizedBox height="10px;"></SizedBox>
+        <Button rightIcon="add">New Question</Button>
       </SideBar>
 
       <QuestionCard>
-        <Card>sjdfskladjfk</Card>
+        <Card>
+          <QuestionTitleInputContainer>
+            <QuestionTitleInput label="Question"></QuestionTitleInput>
+            <QuestionAction>
+              <Button color="danger">
+                <I className="material-icons left">cancel</I>
+                Delete Question
+              </Button>
+            </QuestionAction>
+          </QuestionTitleInputContainer>
+          {[0, 1, 2, 3].map(() => {
+            return (
+              <OptionInputContainer>
+                <I className="material-icons left">menu</I>
+                <OptionInput placeholder="Option"></OptionInput>
+              </OptionInputContainer>
+            );
+          })}
+        </Card>
       </QuestionCard>
     </Container>
   );
--- src/pages/CreateSurvey/styles.js
+++ src/pages/CreateSurvey/styles.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import BaseContainer from "../../components/BaseContainer";
+import TCard from "../../components/Card";
+import TButton from "../../components/Button";
+import Input from "../../components/Input";
 
 export const Container = styled(BaseContainer)`
   display: flex;
@@ -17,7 +20,9 @@ export const SideBar = styled.div`
   justify-content: flex-start;
   min-height: 600px;
   flex: 1;
-  background-color: #ff00000f;
+  /* background-color: #ff00000f; */
+  margin-left: 15px;
+  margin-right: 15px;
 `;
 export const SideBarItem = styled.div`
   display: flex;
@@ -25,13 +30,25 @@ export const SideBarItem = styled.div`
   align-items: center;
   justify-content: space-between;
   border-left: ${props => props.theme.colors.secondary};
+  &:hover {
+    background-color: ${props => `${props.theme.colors.secondary}0f`};
+    cursor: pointer;
+  }
+
+  ${props =>
+    props.selected
+      ? `border-left: 4px solid ${props.theme.colors.secondary};
+        color: ${props.theme.colors.secondary}; 
+  font-weight: bold;`
+      : `border-left: 4px solid ${props.theme.colors.secondary}44;
+      color: ${props.theme.colors.secondary}44;
+  font-weight: normal;`}
 `;
 export const QuestionItemContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   padding: 20px;
-  color: ${props => props.theme.colors.secondary};
 `;
 export const QuestionNumber = styled.p`
   font-size: 1.3em;
@@ -40,14 +57,78 @@ export const QuestionNumber = styled.p`
 export const QuestionTitle = styled.p`
   font-size: 1.3em;
   margin: 0;
-  font-weight: bold;
 `;
 export const QuestionAction = styled.div``;
 
 export const QuestionCard = styled.div`
   display: flex;
   flex: 4;
-  background-color: green;
   min-height: 600px;
-  background-color: #00ff000f;
+`;
+
+export const Card = styled(TCard)`
+  width: 100%;
+`;
+
+export const Button = styled(TButton)`
+  margin: 15px !important;
+  width: auto;
+`;
+
+export const QuestionTitleInput = styled(Input)`
+  font-size: 30px !important;
+  line-height: 30px !important;
+  height: 60px !important;
+  display: flex;
+  flex: 1;
+
+  & input {
+    font-size: 30px;
+    line-height: 30px;
+  }
+  & + label {
+    font-size: 30px;
+    line-height: 10px;
+    /* font-size: 16px; */
+    /* line-height: 30px; */
+    pointer-events: none;
+    padding-bottom: 5px;
+  }
+`;
+
+export const OptionInputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const OptionInput = styled.input`
+  color: ${props => props.theme.colors.secondary};
+
+  background-color: transparent;
+  border: none !important;
+  box-shadow: none !important;
+  margin-bottom: 0 !important;
+
+  &:focus {
+    border: none !important;
+    box-shadow: none !important;
+  }
+`;
+
+export const I = styled.i`
+  color: ${props => props.theme.colors.secondary} !important;
+
+  ${props =>
+    props.danger &&
+    `
+    color: ${props.theme.colors.danger} !important;
+  `}
+`;
+
+export const QuestionTitleInputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
